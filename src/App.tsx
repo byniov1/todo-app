6219d5c0aefe8e041c66a61cb3141dd2e0725f2b
Bynio <bynio788@gmail.com>
2023-07-27 19:55:15 +0200

    feat: multi drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -39,30 +39,65 @@ const DATA = [
 function App() {
 
   const [stores, setStores] = useState(DATA);
-  
-  const handleDragDrop = (result: any) => {
-    const {source, destination, type} = result
 
-    if(!destination) return;
-    
-    if(source.droppableId === destination.droppableId && source.index === destination.index) return 
+  const handleDragAndDrop = (results) => {
+    const { source, destination, type } = results;
 
-    if(type === 'group'){
+    if (!destination) return;
+
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    )
+      return;
+
+    if (type === "group") {
       const reorderedStores = [...stores];
-      
-      const sourceIndex = source.index
-      const desinationIndex = destination.index;
 
-      const [removedStore] = reorderedStores.splice(sourceIndex, 1)
-      reorderedStores.splice(desinationIndex, 0 , removedStore);
+      const storeSourceIndex = source.index;
+      const storeDestinatonIndex = destination.index;
 
-      return setStores(reorderedStores)
+      const [removedStore] = reorderedStores.splice(storeSourceIndex, 1);
+      reorderedStores.splice(storeDestinatonIndex, 0, removedStore);
+
+      return setStores(reorderedStores);
     }
-  }
+    const itemSourceIndex = source.index;
+    const itemDestinationIndex = destination.index;
+
+    const storeSourceIndex = stores.findIndex(
+      (store) => store.id === source.droppableId
+    );
+    const storeDestinationIndex = stores.findIndex(
+      (store) => store.id === destination.droppableId
+    );
+
+    const newSourceItems = [...stores[storeSourceIndex].items];
+    const newDestinationItems =
+      source.droppableId !== destination.droppableId
+        ? [...stores[storeDestinationIndex].items]
+        : newSourceItems;
+
+    const [deletedItem] = newSourceItems.splice(itemSourceIndex, 1);
+    newDestinationItems.splice(itemDestinationIndex, 0, deletedItem);
+
+    const newStores = [...stores];
+
+    newStores[storeSourceIndex] = {
+      ...stores[storeSourceIndex],
+      items: newSourceItems,
+    };
+    newStores[storeDestinationIndex] = {
+      ...stores[storeDestinationIndex],
+      items: newDestinationItems,
+    };
+
+    setStores(newStores);
+  };
 
   return (
     <div>
-      <DragDropContext onDragEnd={handleDragDrop}>
+      <DragDropContext onDragEnd={handleDragAndDrop}>
 
         <div>
           <h1>Shopping List</h1>
@@ -81,7 +116,8 @@ function App() {
                       {...provided.draggableProps}
                       ref={provided.innerRef}
                     >
-                      <h3>{store.name}</h3>
+                      {/* <h3>{store.name}</h3> */}
+                      <StoreList {...store} />
                     </div>
                   )}
                 </Draggable>
@@ -95,6 +131,42 @@ function App() {
 
       </DragDropContext>
     </div>
+  )
+}
+
+interface Props {
+  name: string,
+  items: string[],
+  id: string
+}
+
+function StoreList({ name, items, id }: Props) {
+  return (
+    <Droppable droppableId={id}>
+      {(provided) => (
+        <div>
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            <h3>{name}</h3>
+          </div>
+          <div>
+            {items.map((item: any, index: number) => (
+              <Draggable draggableId={item.id} index={index} key={item.id}>
+                {(provided) => (
+                  <div 
+                    {...provided.dragHandleProps} 
+                    {...provided.draggableProps} 
+                    ref = {provided.innerRef}
+                  >
+                    <h4>{item.name}</h4>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+          </div>
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   )
 }
 
