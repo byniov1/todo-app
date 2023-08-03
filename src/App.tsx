8907de3260d1eeed6a1a400ba660dc34fe3a5205
Bynio <bynio788@gmail.com>
2023-08-03 14:37:54 +0200

    feat: Change in default data

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,31 @@ import './App.css'
 const DATA = [
   {
     id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
-    name: "Walmart",
+    name: "To Do",
     items: [
-      { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "3% Milk" },
-      { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Butter" },
+      { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "Lorem" },
+      { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Ipsum" },
     ],
     tint: 1,
   },
   {
     id: "487f68b4-1746-438c-920e-d67b7df46247",
-    name: "Indigo",
+    name: "In Progress",
     items: [
       {
         id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
         name: "Designing Data Intensive Applications",
       },
-      { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
+      { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Dolor sit" },
     ],
     tint: 2,
   },
   {
     id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
-    name: "Lowes",
+    name: "Done",
     items: [
-      { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Workbench" },
-      { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
+      { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Ament" },
+      { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Consectetur adipiscing" },
     ],
     tint: 3,
   },
